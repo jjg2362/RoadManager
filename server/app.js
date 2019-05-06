31a413d77af82dd5a6a87e7cef183c261410654f
JKCho <jjg2362@naver.com>
2019-05-06 21:17:17 +0900

    made road1 page

--- server/app.js
+++ server/app.js
@@ -3,9 +3,14 @@ const app = express();
 const router = express.Router();
 const path = require('path');
 
-app.use('/static', express.static(path.join(__dirname, '../client', '/static')));
+
+router.get('/road/:id', (req, res) => {
+    app.use('/road/static', express.static(path.join(__dirname, '../client', '/static')));
+    res.sendfile(path.join(__dirname, '../client', `road${req.params.id}.html`));
+})
 
 router.get('/', (req, res) => {
+    app.use('/static', express.static(path.join(__dirname, '../client', '/static')));
     res.sendfile(path.join(__dirname, '../client', 'index.html'));
 })
 
